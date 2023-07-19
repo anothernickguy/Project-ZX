7647b370a42d7d1f2c2fe342edc54ff0fdef1192
anothernickguy <132588165+anothernickguy@users.noreply.github.com>
2023-07-19 12:05:19 -0400

    correciones de nivel y integracion animacion enemigo 2
    solo falta sonido y el jugador

--- Assets/scrpits/EnemyShoot.cs
+++ Assets/scrpits/EnemyShoot.cs
@@ -8,15 +8,17 @@ public class EnemyShoot : MonoBehaviour
     public Transform firePoint;
     public float bulletSpeed = 10f;
     public float shootingInterval = 2f; // Intervalo de tiempo entre disparos
+    public float shootingAnimationDuration = 1f; // Duración de la animación de disparo
     private bool canShoot = true;
     private PlayerController pc;
+    private Animator animator;
     [SerializeField] private float minDistance;
 
     private void Start()
     {
         pc = FindObjectOfType<PlayerController>();
+        animator = GetComponent<Animator>();
         StartCoroutine(ShootCoroutine());
-        
     }
 
     private IEnumerator ShootCoroutine()
@@ -25,11 +27,19 @@ public class EnemyShoot : MonoBehaviour
         {
             if (canShoot)
             {
-                while(Vector2.Distance(pc.transform.position, this.transform.position) > minDistance)
+                while (Vector2.Distance(pc.transform.position, this.transform.position) > minDistance)
                 {
+                    // Cambiar el parámetro "DetectingPlayer" a falso para volver a la animación idle
+                    animator.SetBool("DetectingPlayer", false);
                     yield return null;
                 }
-                
+
+                // Cambiar el parámetro "DetectingPlayer" a verdadero para activar la animación de disparo
+                animator.SetBool("DetectingPlayer", true);
+
+                // Reproducir la animación de disparo
+                animator.SetTrigger("Shoot");
+
                 Shoot();
                 canShoot = false;
                 yield return new WaitForSeconds(shootingInterval);
